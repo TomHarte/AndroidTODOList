a4ea320b12e9777af57c931355971386afc60723
Thomas Harte <thomas.harte@gmail.com>
2014-07-13 21:04:54 -0700

    This is my first faltering step towards using proper objects here.

---
+++ app/src/main/java/com/thomasharte/model/TodoItem.java
@@ -0,0 +1,17 @@
+package com.thomasharte.model;
+
+/**
+ * Created by thomasharte on 13/07/2014.
+ */
+public class TodoItem {
+    private long timestamp;
+    private String description;
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
