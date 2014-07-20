8edbc75f551f250a16117eb695604538f2f9d7a5
Thomas Harte <thomas.harte@gmail.com>
2014-07-20 15:36:07 +0100

    Tweaked the layouts to ensure something other than the edit texts assume default focus, modified the main activity to ensure focus is removed from the edit text when adding an item. Net effect: the soft keyboard isn't half as annoying.

--- app/src/main/java/com/thomasharte/view/MainActivity.java
+++ app/src/main/java/com/thomasharte/view/MainActivity.java
@@ -1,11 +1,13 @@
 package com.thomasharte.view;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -89,6 +91,11 @@ public class MainActivity extends ActionBarActivity {
         todoList.addItem(new TodoItem(etNewItem.getText().toString(), new Date()));
         itemsAdaptor.notifyDataSetChanged();
         etNewItem.setText("");
+
+        // hide the keyboard
+        InputMethodManager imm = (InputMethodManager)getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(etNewItem.getWindowToken(), 0);
     }
 
     @Override
